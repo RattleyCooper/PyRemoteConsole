98c4df6c78060c1506681965a05d5240165eb111
py-am-i <duckpuncherirl@gmail.com>
2018-09-30 16:28:31 -0700

    Updated setup.py with new version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyRemoteConsole",
-    version="0.0.1",
+    version="0.0.2",
     author="py-am-i",
     author_email="duckpuncherirl@gmail.com",
     description="A pure Python 3 package containing hooks for running remote python consoles to interact with running scripts in real time.",
